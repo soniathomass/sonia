48a5071bc3f58e32883b97cc8f5e318663e5fa8c
soniathomass <soniathomas239@gmail.com>
2019-05-14 20:06:03 +0530

    committt

---
+++ factorial.sh
@@ -0,0 +1,15 @@
+fact=1
+echo -e "enter a number"
+read n
+if [ $n -le 0 ] ; then
+echo "invalid number"
+exit
+fi
+if [ $n -gt 0 ] ; then
+for((i=$n;i>=1;i--))
+do
+fact=`expr $fact \* $i`
+done
+fi
+echo "The factorial of $n is $fact"
+
